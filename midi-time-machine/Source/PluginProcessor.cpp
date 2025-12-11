873a4d87e88ecaa0b393549038ea6fc54ca68f7f
maksut <maksut.cagil@gmail.com>
2025-12-11 20:42:48 +0000

    Initial commit with JUCE submodule and project template

---
+++ midi-time-machine/Source/PluginProcessor.cpp
@@ -0,0 +1,129 @@
+#include "PluginProcessor.h"
+#include "PluginEditor.h"
+
+MTMAudioProcessor::MTMAudioProcessor()
+{
+}
+
+MTMAudioProcessor::~MTMAudioProcessor()
+{
+}
+
+const juce::String MTMAudioProcessor::getName() const
+{
+    return "MIDI Time Machine";
+}
+
+bool MTMAudioProcessor::acceptsMidi() const
+{
+    return true;
+}
+
+bool MTMAudioProcessor::producesMidi() const
+{
+    return true;
+}
+
+bool MTMAudioProcessor::isMidiEffect() const
+{
+    return true;
+}
+
+double MTMAudioProcessor::getTailLengthSeconds() const
+{
+    return 0.0;
+}
+
+int MTMAudioProcessor::getNumPrograms()
+{
+    return 1;   // NB: some hosts don't cope very well if you tell them there are 0 programs,
+                // so this should be at least 1, even if you're not really implementing programs.
+}
+
+int MTMAudioProcessor::getCurrentProgram()
+{
+    return 0;
+}
+
+void MTMAudioProcessor::setCurrentProgram (int index)
+{
+}
+
+const juce::String MTMAudioProcessor::getProgramName (int index)
+{
+    return {};
+}
+
+void MTMAudioProcessor::changeProgramName (int index, const juce::String& newName)
+{
+}
+
+void MTMAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlock)
+{
+    // Use this method as the place to do any pre-playback
+    // initialisation that you need..
+}
+
+void MTMAudioProcessor::releaseResources()
+{
+    // When playback stops, you can use this as an opportunity to free up any
+    // spare memory, etc.
+}
+
+void MTMAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, juce::MidiBuffer& midiMessages)
+{
+    juce::ScopedNoDenormals noDenormals;
+    auto totalNumInputChannels  = getTotalNumInputChannels();
+    auto totalNumOutputChannels = getTotalNumOutputChannels();
+
+    // In case we have more outputs than inputs, this code clears any output
+    // channels that didn't contain input data, (because these aren't
+    // guaranteed to be empty - they may contain garbage).
+    // This is here to avoid people getting screaming feedback
+    // when they first compile a plugin, but obviously you don't need to keep
+    // this code if your algorithm always overwrites all the output channels.
+    for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
+        buffer.clear (i, 0, buffer.getNumSamples());
+
+    // This is the place where you'd normally do the guts of your plugin's
+    // audio processing...
+    // Make sure to reset the state if your inner loop is processing
+    // the samples and the outer loop is handling the channels.
+    // Alternatively, you can process the samples with the channels
+    // interleaved by keeping the same state.
+    for (int channel = 0; channel < totalNumInputChannels; ++channel)
+    {
+        auto* channelData = buffer.getWritePointer (channel);
+
+        // ..do something to the data...
+    }
+}
+
+bool MTMAudioProcessor::hasEditor() const
+{
+    return true;
+}
+
+juce::AudioProcessorEditor* MTMAudioProcessor::createEditor()
+{
+    return new MTMAudioProcessorEditor (*this);
+}
+
+void MTMAudioProcessor::getStateInformation (juce::MemoryBlock& destData)
+{
+    // You should use this method to store your parameters in the memory block.
+    // You could do that either as raw data, or use the XML or ValueTree classes
+    // as intermediaries to make it easy to save and load complex data.
+}
+
+void MTMAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
+{
+    // You should use this method to restore your parameters from this memory block,
+    // whose contents will have been created by the getStateInformation() call.
+}
+
+// This creates new instances of the plugin..
+juce::AudioProcessor* JUCE_CALLTYPE createPluginFilter()
+{
+    return new MTMAudioProcessor();
+}
